c8f921e1535ae631e6886491c1962d35d82fa19a
pagongamedev <pagongamedev@gmail.com>
2019-05-10 08:10:55 +0700

    add babel and eslint

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,28 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "./build/"),
     filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: ["@babel/plugin-proposal-class-properties"]
+            }
+          },
+          {
+            loader: "eslint-loader",
+            options: {
+              // eslint options (if necessary)
+            }
+          }
+        ]
+      }
+    ]
   }
 };
